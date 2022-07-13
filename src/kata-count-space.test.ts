8df7bd4309707c6f1d2985e6a8810f0c2af18878
purpurlu <laurazurbayeva@gmail.com>
2022-07-13 10:17:49 +0100

    check format

--- src/kata-count-space.test.ts
+++ src/kata-count-space.test.ts
@@ -4,7 +4,6 @@ test("Counts number of space characters", () => {
   expect(countSpace("Hello world")).toBe(1);
   expect(countSpace("  ")).toBe(2);
   expect(countSpace("")).toBe(0);
-  expect(countSpace('2 4 6 7')).toBe(3);
-  expect(countSpace('[2, 5, 6, 8]')).toBe(3);
-  
+  expect(countSpace("2 4 6 7")).toBe(3);
+  expect(countSpace("[2, 5, 6, 8]")).toBe(3);
 });
--- src/kata-count-space.ts
+++ src/kata-count-space.ts
@@ -3,16 +3,14 @@
  * @param inputString - the word, where space chasracter should be counted
  * @returns the number of space character
  */
- function countSpace(inputString: string): number {
-    let spaceNumber = 0;
-    for (let CHARACTER of inputString) {
-      if (CHARACTER === ' ') {
-        spaceNumber++;
-      }
+function countSpace(inputString: string): number {
+  let spaceNumber = 0;
+  for (let CHARACTER of inputString) {
+    if (CHARACTER === " ") {
+      spaceNumber++;
     }
-    return spaceNumber
   }
+  return spaceNumber;
+}
 
-  
-  export default countSpace;
-  
+export default countSpace;
